1897064bb54401385d561dcaeb2549f6b3ec421c
Jatin Kumawat <47708378+jatink1@users.noreply.github.com>
2020-04-07 20:43:47 +0530

    user authentication added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const con = mysql.createConnection({
 });
 
 
-//route for new user
+//route for adding a new user
 app.post('/createUser',(req,res)=>{
     var name = req.body.name;
     var email = req.body.email;
@@ -39,13 +39,59 @@ app.post('/createUser',(req,res)=>{
     });
 });
 
+//User authentication
+app.post('/userAuth',(req,res)=>{
+  var email = req.body.email;
+  var password = req.bpdy.password;
+
+  con.query('SELECT * FROM userinfo WHERE email=("' + email + '") AND password=("' + password + '")', (err, results) => {
+
+   if(results.length == 0){
+     console.log("Wrong credentials");
+   res.send({
+     success: false,
+     message: "Invalid credentials"
+   });
+   }
+   else{
+   //myuserid = results[0].userid; //is used in useridPost & exchange to show posts&exchange of this user
+   console.log("Current user info:", results);
+   console.log("User id:", results[0].id, "\nEmail:", results[0].email, "\nPassword:", results[0].password);
+
+  var id = results[0].id;
+  var name = results[0].name;
+ var email = results[0].email;
+
+   if (results.length > 0) {
+     console.log("user logged");
+     res.send({
+       success: true,
+       status: 200,
+       id: id,
+       name: name,
+       email: email,
+       message: "correct credentials, login succesful"
+     });
+   }
+   else {
+     res.send({
+       success: false,
+       status: 204,
+       message: "Incorrect credentials"
+     });
+   }
+ };
+ });
+});
+
+
 //route for adding new subject
 app.post('/addSubject', (req, res) => {
     var subName = req.body.subName;
     var userId = req.body.userId;
 
     query = 'INSERT INTO subjectlist (subjectName, userId) VALUES ("' + subName + '", "' + userId + '") ';
-    
+
     con.query(query, (err, results) => {
         if (err) res.send(err);
         else {
@@ -62,7 +108,7 @@ app.post('/addUnit', (req, res) => {
     var subjectName = req.body.subName;
 
     query = 'INSERT INTO unitlist (subjectName,userId, unitName) VALUES ("' + subjectName + '", "' + userId + '", "' + unitName + '") ';
-    
+
     con.query(query, (err, results) => {
         if (err) res.send(err);
         else {
@@ -73,7 +119,22 @@ app.post('/addUnit', (req, res) => {
 });
 
 //route for adding time
+app.post('/addTime',(req,res)=>{
+  var userId = req.body.userId;
+  var subjectName = req.bode.subjectName;
+  var unitName = req.body.unitName;
+  var time = req.body.time;
 
+  query = 'INSERT INTO timespent (userId,subjectName,unitName,timeSpentUnit) VALUES ("'+userId+'", "'+subjectName+'", "'+unitName+'", "'+time+'")';
+
+  con.query(query,(err,results)=>{
+    if(err) res.send(err);
+    else{
+      console.log("Time added");
+      res.send("Time added");
+    };
+  });
+});
 
 //route for viewing current user details
 app.post('/getUser', (req, res) => {
